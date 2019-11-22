9238370a965c69a65c3bba72800fe41d1f9a4d23
thirumal2901 <thirumalm022@gmail.com>
2019-11-22 11:59:48 +0530

    4 files modified, 1 added-header/footer(completed)

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   constructor(
